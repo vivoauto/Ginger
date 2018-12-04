628eb066c4f012d7e55ca30d814201cd6bd16bfc
Kethe, Ravi (rk277a) <ravi.kethe@amdocs.com>
2018-12-04 11:35:55 -0600

    Reverting type fix

--- Ginger/Ginger/Run/BusinessFlowRunConfigurationsPage.xaml.cs
+++ Ginger/Ginger/Run/BusinessFlowRunConfigurationsPage.xaml.cs
@@ -212,7 +212,7 @@ namespace Ginger.Run
                                     if (copiedVar.Formula != originalFormula || copiedVar.Value != originalValue) //variable was changed
                                     {
                                         copiedVar.VarValChanged = true;
-                                        copiedVar.DifferentFromOrigin = true;
+                                        copiedVar.DiffrentFromOrigin = true;
                                     }
                                 }
                             }
@@ -285,7 +285,7 @@ namespace Ginger.Run
             if (varToEdit.Formula != originalFormula || varToEdit.Value != originalValue)//variable was changed
             {
                 varToEdit.VarValChanged = true;
-                varToEdit.DifferentFromOrigin = true;
+                varToEdit.DiffrentFromOrigin = true;
             }
             UpdateEditVariablesTabVisual();
         }
@@ -349,7 +349,7 @@ namespace Ginger.Run
         private void SetAutoDescription()
         {
             string autoDesc = string.Empty;
-            List<VariableBase> bfVariables = (mBusinessFlow.GetBFandActivitiesVariabeles(true)).Where(var => var.GetType() == typeof(VariableSelectionList) || var.DifferentFromOrigin == true || string.IsNullOrEmpty(var.MappedOutputVariable)==false || string.IsNullOrEmpty(var.MappedOutputValue) == false).ToList(); ;
+            List<VariableBase> bfVariables = (mBusinessFlow.GetBFandActivitiesVariabeles(true)).Where(var => var.GetType() == typeof(VariableSelectionList) || var.DiffrentFromOrigin == true || string.IsNullOrEmpty(var.MappedOutputVariable)==false || string.IsNullOrEmpty(var.MappedOutputValue) == false).ToList(); ;
             if (bfVariables != null && bfVariables.Count > 0)
             {
                 autoDesc = "Running Configurations: ";
@@ -433,7 +433,7 @@ namespace Ginger.Run
         {
             int count = 0;
             foreach (VariableBase var in mBusinessFlow.GetBFandActivitiesVariabeles(true,true))
-                if (var.DifferentFromOrigin == true)
+                if (var.DiffrentFromOrigin == true)
                     count++;
 
             if (count > 0)
